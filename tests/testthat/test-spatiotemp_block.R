844385c8afb7bbec052abe341bfc0b0b96843a08
RachelDobson <75269356+RachelDobson@users.noreply.github.com>
2022-11-29 09:32:16 +0000

    Fix test

--- tests/testthat/test-spatiotemp_block.R
+++ tests/testthat/test-spatiotemp_block.R
@@ -4,23 +4,23 @@ rm(sample_model_data)
 biome_layer <- raster::raster()
 raster::extent(biome_layer)<-c(11.71845, 40.85081, -47.89832, -4.428839)
 raster::res(biome_layer)<-10
-values(biome_layer) <- 1:raster::ncell(biome_layer)
+raster::values(biome_layer) <- 1:raster::ncell(biome_layer)
 
 
 test_that("stops if no occ.data provided", {
-  expect_error(spatiotemp_block(spatial.layer=biome_layer,spatial.split.degrees=3,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data)[9:12],n.blocks=10))})
+  expect_error(spatiotemp_block(spatial.layer=biome_layer,spatial.split.degrees=3,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data2)[9:12],n.blocks=10))})
 
 test_that("stops if no vars.to.block.by provided", {
   expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,spatial.split.degrees=3,temporal.block=c("month"),n.blocks=10))})
 
 test_that("stops if spatial.layer not class RasterLayer", {
-  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=c(0,20,-20,30),spatial.split.degrees=3,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data)[9:12],n.blocks=10))})
+  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=c(0,20,-20,30),spatial.split.degrees=3,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data2)[9:12],n.blocks=10))})
 
 test_that("stops if spatial.split.degrees not class numeric", {
-  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,spatial.split.degrees="three",temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data)[9:12],n.blocks=10))})
+  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,spatial.split.degrees="three",temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data2)[9:12],n.blocks=10))})
 
 test_that("stops if spatial.layer given but no spatial.split.degrees", {
-  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data)[9:12],n.blocks=10))})
+  expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,temporal.block=c("month"),vars.to.block.by=colnames(sample_model_data2)[9:12],n.blocks=10))})
 
 test_that("stops if temporal.block not accepted", {
   expect_error(spatiotemp_block(occ.data = sample_model_data2,spatial.layer=biome_layer,spatial.split.degrees=3,temporal.block=c("decadal"),vars.to.block.by=colnames(sample_model_data2)[9:12],n.blocks=10))})
